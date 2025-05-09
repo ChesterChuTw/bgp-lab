d4f8fe00424e042c34a3cb7ea2c3e9d4efeda210
Chester Chu <chester850117@gmail.com>
2025-05-09 05:12:31 -0400

    demo1: delete useless files

--- demo1-ebgp-basic/clearup.sh
+++
@@ -1,5 +0,0 @@
-docker rm -f frr-100-r10
-docker rm -f frr-200-r10
-docker network rm demo1-ebgp-basic_net100
-docker network rm demo1-ebgp-basic_net101
-docker network rm demo1-ebgp-basic_net200
--- demo2-ibgp-ospf/clearup.sh
+++
@@ -1,3 +0,0 @@
-docker rm -f frr-100-r10
-docker rm -f frr-100-r11
-docker network rm demo2-ibgp-ospf_net100
